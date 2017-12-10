d443f29910a46471e76b64a7e574409d96cf094f
Nicolas Bonnet <nicolas.bonnet07@gmail.com>
2017-12-10 19:12:30 +0100

    update task model

--- apps/V5.5/app/Task.php
+++ apps/V5.5/app/Task.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'task';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'begin_date', 'end_date'];
 }
